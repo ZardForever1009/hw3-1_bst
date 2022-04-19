8c2ebd1925234511e79d340cd6ae185dcc4825cf
zardfoever <87600155+ZardForever1009@users.noreply.github.com>
2022-04-19 14:04:47 +0800

    Update hw3-1.cpp

--- hw3-1.cpp
+++ hw3-1.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+static int node_count=0;
+
 // tree node initializtion
 class Node{
 public:
@@ -15,9 +17,84 @@ public:
 	Node* right=nullptr;
 };
 
+// check if data is invalid
+bool invalid_data(string data){
+	// check if non digit exist
+	for(int i=0;i<data.size();i++){
+		if(!isdigit(data[i]))return true;
+		else;
+	}
+	// check if data overflow
+	if(stoi(data)>100)return true;
+	else return false;
+}
+
+// Search function (TRUE: exist)
+bool Search(Node* root, string data){
+	// prevent invalid search options
+	if(invalid_data(data)){
+		cout<<"Search wrong data\n";
+		return false;
+	}
+	if(root==nullptr)return false; // impossible for data to store in the bst
+	// start searching
+	if(root->data==stoi(data))return true;
+	else{
+		if(root->left!=nullptr)Search(root->left, data);
+		if(root->right!=nullptr)Search(root->right, data);
+	}
+	return false;
+}
+
 // insert function
 void Insert(Node*& root, string data){
-	
+	// prevent invalid insert options
+	if(invalid_data(data)){
+		cout<<"Insert wrong data\n";
+		return;
+	}
+	// root is nullptr
+	if(root==nullptr){
+		root=new Node();
+		root->data=stoi(data);
+		return;
+	}
+	// root is NOT nullptr && same data already exist
+	else if(Search(root, data)){
+		cout<<"This data is in BST\n";
+		return;
+	}
+	// root is NOT nullptr && No same data exist in bst
+	// insert node to its position
+	else{
+		Node* restore_root=root;
+		// go right
+		if(stoi(data)>root->data){
+			if(root->right==nullptr){ // create new node
+				Node* new_node=new Node();
+				new_node->data=stoi(data);
+				root->right=new_node;
+				return;
+			}
+			else{
+				Insert(root->right, data);
+			}
+		}
+		// go left
+		else{
+			if(root->left==nullptr){
+				Node* new_node=new Node();
+				new_node->data=stoi(data);
+				root->left=new_node;
+				return;
+			}
+			else{
+				Insert(root->left, data);
+			}
+		}
+		root=restore_root;
+	}
+	return;
 }
 
 // delete function
@@ -26,28 +103,27 @@ void Delete(Node*& root, string data){
 }
 
 // height of tree
-int Height(Node*& root){
+int Height(Node* root){
 	
 }
 
 // print out tree by level order
-void Print(Node*& root){
-	
-}
-
-// Search function
-void Search(Node*& root, string data){
+void Print(Node* root){
 	
 }
 
 // Number function
-int Number(Node*& root){
-	
+int Number(Node* root){
+	node_count++;
+	if(root==nullptr)return 0;
+	if(root->left!=nullptr)Number(root->left);
+	if(root->right!=nullptr)Number(root->right);
+	return node_count;
 }
 
 // combination of all instructions
 void bst(){
-	Node* root=new Node();
+	Node* root=nullptr;
 	string action, data;
 	while(cin>>action){
 		if(action=="Insert"){
@@ -69,7 +145,7 @@ void bst(){
 			Search(root, data);
 		}
 		else if(action=="Number"){
-			Number(root);
+			cout<<Number(root)<<endl;
 		}
 		else{
 			cout<<"Wrong instruction"<<endl;
